5fe79aaca30750fc0282f52025632eb52a3a2602
Nicolas <nthomas@robotikdose.com>
2013-10-09 17:48:48 +0200

    09101702

--- www/js/camera.js
+++ www/js/camera.js
@@ -33,6 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        checkConnection();
         //app.receivedEvent('deviceready');
         // Do cool things here...
     },
@@ -152,3 +153,20 @@ function onFail(message) {
     function vibrate() {
         navigator.notification.vibrate(2000);
     }
+    
+    function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
+
